9ca9b077823c30d8e01a7fa020c4d908314ad8db
slim <ouertani@gmail.com>
2025-01-07 16:27:42 +0100

    feat: add ghostty

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,9 +6,9 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown", "graphql" },
-  callback = function()
-    vim.opt.conceallevel = 0
-  end,
-})
+--vim.api.nvim_create_autocmd("FileType", {
+--  pattern = { "json", "jsonc", "markdown", "graphql" },
+--  callback = function()
+--    vim.opt.conceallevel = 0
+--  end,
+--})
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+opt.guicursor = {
+  "a:block",
+  "n-v-c:block-Cursor", -- Normal, Visual, Command mode: block cursor (_)
+  "i-ci-ve:ver25", -- Insert, Command-line Insert, Visual Exclusive mode: vertical bar
+  "r-cr:hor20", -- Replace and Command-line Replace mode: horizontal underscore
+  "o:hor50", -- Operator-pending mode: horizontal bar
+  "sm:block-blinkwait175-blinkoff150-blinkon175", -- Cursor blinking in showmatch mode
+}
