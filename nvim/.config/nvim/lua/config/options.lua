e1202ec0d1e70380cf493a6b93ae1a166d6b5875
slim <ouertani@gmail.com>
2025-01-13 09:16:48 +0100

    feat: add ghostty

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,1 @@
-local opt = vim.opt
-opt.guicursor = {
-  "a:block",
-  "n-v-c:block-Cursor", -- Normal, Visual, Command mode: block cursor (_)
-  "i-ci-ve:ver25", -- Insert, Command-line Insert, Visual Exclusive mode: vertical bar
-  "r-cr:hor20", -- Replace and Command-line Replace mode: horizontal underscore
-  "o:hor50", -- Operator-pending mode: horizontal bar
-  "sm:block-blinkwait175-blinkoff150-blinkon175", -- Cursor blinking in showmatch mode
-}
+vim.opt.guifont = "JetBrainsMono Nerd Font:h14"
